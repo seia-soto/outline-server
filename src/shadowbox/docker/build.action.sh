da1a06b8f9426f85546fd0f53f0247b3051fea2c
seia-soto <seia@outlook.kr>
2022-01-25 02:13:25 +0900

    Add missing buildx command

--- src/shadowbox/docker/build.action.sh
+++ src/shadowbox/docker/build.action.sh
@@ -24,7 +24,7 @@ export DOCKER_BUILDKIT=1
 readonly NODE_IMAGE="node:16.12-alpine3.14"
 
 # Use Docker Buildx for building multi-platform images.
-docker buildx \
+docker buildx build \
     --platform="linux/amd64,linux/arm64,linux/arm/v7" \
     --push \
     --force-rm \
